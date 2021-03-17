09c17ff2d8bb5e917f02b785d2d31b7902b1893b
jabenya05@gmail.com <Rowad678>
2021-03-17 16:33:27 +0200

    CameraX

---
+++ app/src/main/java/com/test/seeu/Camera.java
@@ -0,0 +1,42 @@
+package com.test.seeu;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+public class Camera extends AppCompatActivity {
+    private static final String[] CAMERA_PERMISSION = new String[]{Manifest.permission.CAMERA};
+    private static final int CAMERA_REQUEST_CODE = 10;
+    private Activity context;
+
+    public Camera(Activity context){
+        this.context = context;
+    }
+
+    public void checkPermissionCamera() {
+        if (hasCameraPermission(context)) {
+            enableCamera(context);
+        } else {
+    requestPermission(context);
+        }
+    }
+
+    private void requestPermission(Activity context) {
+        ActivityCompat.requestPermissions(context, CAMERA_PERMISSION, CAMERA_REQUEST_CODE);
+    }
+
+    private void enableCamera(Activity context) {
+        Intent intent = new Intent(context, CameraActivity.class);
+        startActivity(intent);
+
+    }
+
+    private boolean hasCameraPermission(Activity context) {
+        return ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
+    }
+}
--- app/src/main/java/com/test/seeu/MainActivity.java
+++ app/src/main/java/com/test/seeu/MainActivity.java
@@ -1,14 +1,48 @@
 package com.test.seeu;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnConnectCamera;
+    private static final String[] CAMERA_PERMISSION = new String[]{Manifest.permission.CAMERA};
+    private static final int CAMERA_REQUEST_CODE = 10;
+    Camera camera = new Camera(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnConnectCamera = findViewById(R.id.btnConnectCamera);
+        btnConnectCamera.setOnClickListener(v -> {
+
+            camera.checkPermissionCamera();
+          /*  if (hasCameraPermission()) {
+                enableCamera();
+            } else {
+                requestPermission();
+            }*/
+        });
     }
+
+ /*   private void requestPermission() {
+        ActivityCompat.requestPermissions(this, CAMERA_PERMISSION, CAMERA_REQUEST_CODE);
+    }
+
+    private void enableCamera() {
+        Intent intent = new Intent(this, CameraActivity.class);
+        startActivity(intent);
+    }
+
+    private boolean hasCameraPermission() {
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
+    }*/
 }
